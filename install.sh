9722b5d5b80a4fd57b0b6588edbaf7bf88a1d977
Nico T <94549811+nat0321@users.noreply.github.com>
2026-09-18 11:22:45 -0500

    Update install file

--- install.sh
+++ install.sh
@@ -146,17 +146,17 @@ if [[ "$1" == "test" ]] || [[ -n "$git_source" ]]; then
     cd "$gpath/git"
     TAR_VERSION="$(cat version)_dirty"
 else
-    VERSION_NEW=$(curl --silent --show-error "https://raw.githubusercontent.com/NoDak-Pilot/tar1090/master/version")
-    if  [[ "$(cat "$gpath/git/version" 2>/dev/null)" != "$VERSION_NEW" ]]; then
-        if ! getGIT "$repo" "master" "$gpath/git"; then
-            echo "Unable to download files, exiting! (Maybe try again?)"
-            exit 1
-        fi
+    echo "Updating tar1090..."
+    if ! getGIT "$repo" "master" "$gpath/git"; then
+        echo "Unable to download files, exiting! (Maybe try again?)"
+        exit 1
     fi
+
     if ! cd "$gpath/git"; then
         echo "Unable to download files, exiting! (Maybe try again?)"
         exit 1
     fi
+
     TAR_VERSION="$(cat version)"
 fi
 
